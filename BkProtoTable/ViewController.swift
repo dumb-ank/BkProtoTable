c7db4c75bc0b18d487aa6947fe6a63c11888cb08
Ankur Bose <Ank@ankurs-macbook-pro.local>
2018-03-20 04:30:23 +0530

    Initial Commit

---
+++ BkProtoTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BkProtoTable
+//
+//  Created by Ankur Bose on 20/03/18.
+//  Copyright © 2018 Ankur Bose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
